56c5e2d298fd351401654a2afb225455f1934921
Leonardo H. Keler <leonardo.keler.14@gmail.com>
2025-08-15 11:26:15 -0300

    feat: Associate posts with User entity

--- src/main/java/com/github/keler147/workshopmongo/domain/User.java
+++ src/main/java/com/github/keler147/workshopmongo/domain/User.java
@@ -1,8 +1,10 @@
 package com.github.keler147.workshopmongo.domain;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
-import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -13,14 +15,15 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts =  new ArrayList<>();
+
     public User() {
     }
     public User(String id, String name, String email) {
         this.id = id;
         this.name = name;
-        if (!email.contains("@")) {
-            throw new InvalidParameterException("Email invalid!");
-        } else { this.email = email; }
+        this.email = email;
     }
 
     public String getEmail() {
@@ -40,6 +43,12 @@ public class User implements Serializable {
     }
     public void setName(String name) {
         this.name = name;
+    }
+    public List<Post> getPosts() {
+        return posts;
+    }
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
